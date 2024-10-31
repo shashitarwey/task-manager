c50f2177fd1569205f025e1bc9e326a9b5a91aed
Shashikant Kumar <shashikantkumar@gofynd.com>
2024-10-31 12:39:08 +0530

    task-manager-app

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import {createRouter, createWebHistory} from 'vue-router';
+import About from '../view/About'
+import Home from '../view/Home'
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router;
---
+++ src/view/About.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h3>Version 1.0.0</h3>
+        <router-link to="/">Go To Home</router-link>
+    </div>
+</template>
